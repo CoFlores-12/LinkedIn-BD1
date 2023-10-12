1802f309b8be6828dcb1ab441296e9a3f481bd8c
CoFlores-12 <obethflores2014@gmail.com>
2023-10-12 10:51:37 -0600

    database connection

--- database/migrations/2023_10_12_000000_create_users_table.php
+++ database/migrations/2023_10_12_000000_create_users_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('info')->nullable();
+            $table->string('photo')->nullable();
+            $table->string('banner')->nullable();
+            $table->string('location')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/login', function () {
+    return 'Hello World';
+});
+Route::get('/user/{id}', function ($id) {
+    return 'Hello user '.$id;
+});
+Route::post('test', function(Request $request) {
+    return $request;
 });
